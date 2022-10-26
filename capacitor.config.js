d4a65524e8be0dcd7b33eeded8de8bcd6277f531
tobey.allison <tobey.allison@emit.technology>
2022-10-26 20:18:38 +0800

    update

---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var config = {
+    appId: 'technology.emit.pins',
+    appName: 'EMIT PINs',
+    webDir: 'build',
+    bundledWebRuntime: false
+};
+exports.default = config;
